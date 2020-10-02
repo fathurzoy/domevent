645b498ad6e1101849cf854b1fdd813a10a09fc0
fathurzoy <fathurrahmanf123@gmail.com>
2020-10-02 10:50:43 +0700

    update gallery domevent

--- main.js
+++ main.js
@@ -27,3 +27,16 @@ const hideAboutAlert = ()=>{
     // kita atur STYLE dari element tersebut jadikan di tersembunyi
     aboutAlert.style.display = 'none'
 }
+
+// kita akan buat element HTML menggunakan javascript
+let gallery = document.getElementById('container-gallery')
+let img = 13
+
+// INSERT HTML SYNTAX into GALLERY
+// kelebihan menggunakan js element lebih bisa dinamis
+
+for(let i = 0; i < 36; i++){
+    gallery.innerHTML += `
+        <img src="https://picsum.photos/seed/${img++}/300" />
+    `
+}
